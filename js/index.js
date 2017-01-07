c4bd59a027a786b1215fee3714e40dee2ca88981
a-kile <udayk8139@hotmail.com>
2017-01-07 21:21:33 +0530

    Adding small changes to hobbies section which include changing the li style and also fixing the blackout effect to coever whole page

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 $('document').ready(function(){
   $('#blackout').animate({
     opacity: 0,
+    height: 0
   }, 2000)
+
   $( "#about" ).animate({
     opacity: 1,
     top: "+=50",
   }, 1000, function() {
     // Animation complete.
   });
+
 })
 
+function accordian(obj, btns){
+  for(var i=0; i < btns.length; i++){
+    btns[i].addEventListener("Click");
+    })
+  }
+}
+
+accordian("d", document.getElementsByClassName["accord"][0].childNodes)
 
 function generateParticles(parent, amount){
   for (var i=0; i < amount; i++){
     var particle = document.createElement("span");
     parent.appendChild(particle);
-    particle.innerText = "DSJKdslkjd";
     particle.className = "particle";
   }
 }
