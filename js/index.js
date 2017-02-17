af442ce5150d2c71db04a3cf62392d0bba3c5b96
a-kile <udayk8139@hotmail.com>
2017-02-17 16:52:40 +0530

    Added fixed header functionality, still glitchy

--- js/index.js
+++ js/index.js
@@ -11,3 +11,32 @@ $('document').ready(function(){
     // Animation complete.
   });
 })
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+function replaceString(str, query, after) {
+  var newStr = str.split(" ");
+  for (var i=0; i < newStr.length; i++){
+    if (newStr[i].toLowerCase() == query.toLowerCase()){
+      newStr.splice(i, 1, after);
+    }
+  }
+  return newStr.join(" ");
+}
+
+var headerTrigger = 126;
+var projectsTrigger = 1000;
+
+window.addEventListener("scroll", function(e){
+  if (window.scrollY >= headerTrigger){
+    if (!hasClass(document.getElementsByClassName("header")[0], "header-f")){
+      document.getElementsByClassName("header")[0].className += " header-f";
+    }
+  } else if (window.scrollY <= headerTrigger){
+    if (hasClass(document.getElementsByClassName("header")[0], "header-f")){
+      document.getElementsByClassName("header")[0].className = replaceString(document.getElementsByClassName("header")[0].className, "header-f", "");
+    }
+  }
+});
