5853ae9c3053cb316d8d8560ffffc3a1420abddc
buoyantair <buoyantair@gmail.com>
2017-02-27 12:17:16 +0530

    small changes

--- js/project_slideshow.js
+++ js/project_slideshow.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("start");
 var proj = document.getElementById("fcc");
-
+var projects = document.getElementsByClassName("projects")[0];
 
 
 button.addEventListener("mouseover", function(){
@@ -10,9 +10,17 @@ button.addEventListener("mouseover", function(){
 });
 
 button.addEventListener("click", function(){
+
+    // Animate the header out of the way
     proj_container.style.position = "relative";
     proj_container.animate({
         left: ["0px", "-1000px"]
     }, 700)
     proj_container.style.left = "-1000px";
+
+    // Display projects
+    projects.animate({
+        opacity: [0, 1],
+    }, 3000)
+    projects.style.opacity = 1;
 });
