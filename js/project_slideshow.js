47bd5f2a209aa207162a21e29f20025ce0f22435
buoyantair <buoyantair@gmail.com>
2017-02-27 13:14:36 +0530

    Added static project section, I'll add projects later on

--- js/project_slideshow.js
+++ js/project_slideshow.js
@@ -8,19 +8,3 @@ button.addEventListener("mouseover", function(){
         background: ["red", "blue"]
     }, 2000)
 });
-
-button.addEventListener("click", function(){
-
-    // Animate the header out of the way
-    proj_container.style.position = "relative";
-    proj_container.animate({
-        left: ["0px", "-1000px"]
-    }, 700)
-    proj_container.style.left = "-1000px";
-
-    // Display projects
-    projects.animate({
-        opacity: [0, 1],
-    }, 3000)
-    projects.style.opacity = 1;
-});
