86898681b0d975570cec50df218522cc2727944c
buoyantair <buoyantair@gmail.com>
2017-02-27 11:59:24 +0530

    Reverted back to simple mode at projects section

--- js/project_slideshow.js
+++ js/project_slideshow.js
@@ -1,6 +1,8 @@
-var button = document.getElementById("next");
+var button = document.getElementById("start");
 var proj = document.getElementById("fcc");
-var start = document.getElementById("start");
+
+
+
 button.addEventListener("mouseover", function(){
     button.animate({
         background: ["red", "blue"]
@@ -8,9 +10,9 @@ button.addEventListener("mouseover", function(){
 });
 
 button.addEventListener("click", function(){
-    start.style.position = "relative";
-    start.animate({
+    proj_container.style.position = "relative";
+    proj_container.animate({
         left: ["0px", "-1000px"]
-    }, 1000)
-    start.style.left = "-1000px";
+    }, 700)
+    proj_container.style.left = "-1000px";
 });
