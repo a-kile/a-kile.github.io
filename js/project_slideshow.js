33591ca75e3b31c4a5ec6d0d26141c76affe57fa
buoyantair <buoyantair@gmail.com>
2017-03-01 14:57:07 +0530

    Added a fade out effect for project header

--- js/project_slideshow.js
+++ js/project_slideshow.js
@@ -1,10 +1,16 @@
 var button = document.getElementById("start");
-var proj = document.getElementById("fcc");
-var projects = document.getElementsByClassName("projects")[0];
+var proj = document.getElementById("proj_container");
 
 
 button.addEventListener("mouseover", function(){
     button.animate({
         background: ["red", "blue"]
-    }, 2000)
+    }, 2000);
+});
+
+button.addEventListener("click", function(){
+    proj.animate({
+        transform: ["rotate3d(0,0,0, 0deg)", "rotate3d(1,1,0, 15deg) scale(30)"],
+    }, 4000);
+    proj.style.transform = "rotate3d(1,1,0, 15deg) scale(30)";
 });
