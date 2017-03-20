409b0f58b5c78a9cab5fd543b945c001517c18bf
buoyantair <buoyantair@gmail.com>
2017-03-20 15:02:22 +0530

    Finished with the dash board

--- projects/fcc/tic-tac-toe/js/tic-tac-toe.js
+++ projects/fcc/tic-tac-toe/js/tic-tac-toe.js
@@ -2,18 +2,64 @@ var gameboard = document.getElementById("game");
 var boxes = document.getElementsByClassName("box");
 var dash = document.getElementById("dash")
 var closeDash = document.getElementById("close");
+var heads = {
+    obj: null,
+    checked: false
+};
+heads.obj = document.getElementById("heads");
+var tails = {
+    obj: null,
+    checked: false
+};
+tails.obj = document.getElementById("tails");
+var colors = {
+    defSelCol: "rgba(50, 48, 49, 0.2)",
+    red: "#E3655B",
+    green: "#8CB369"
+}
+var player;
 
-for(box in boxes){
-    if(typeof boxes[box] == "object"){
-        boxes[box].addEventListener("mouseenter", function(e){
-            e.srcElement.style.background = "rgb(" + Math.floor(Math.random() * 255) + ","+ Math.floor(Math.random() * 255) +","+ Math.floor(Math.random() * 255) + ")";   ;
+var coinSelected = false;
+
+heads.obj.addEventListener("click", function() {
+    coinSelected = true;
+    if (!heads.checked) {
+        heads.checked = true;
+        player = "heads";
+        heads.obj.style.background = colors.red;
+        tails.obj.style.background = colors.defSelCol;
+    } else if (heads.checked) {
+        heads.checked = false;;
+        heads.obj.style.background = colors.defSelCol;;
+    }
+})
+
+tails.obj.addEventListener("click", function() {
+    coinSelected = true;
+    if (!tails.checked) {
+        tails.checked = true;
+        player = "tails";
+        tails.obj.style.background = colors.green;
+        heads.obj.style.background = colors.defSelCol;
+    } else if (tails.checked) {
+        tails.checked = false;
+        tails.obj.style.background = colors.defSelCol;;
+    }
+
+})
+
+for (box in boxes) {
+    if (typeof boxes[box] == "object") {
+        boxes[box].addEventListener("mouseenter", function(e) {
+            var col = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+            e.srcElement.style.background = col;
         });
-        boxes[box].addEventListener("mouseleave", function(e){
+        boxes[box].addEventListener("mouseleave", function(e) {
             e.srcElement.style.background = "white";
         });
     }
 }
 
-closeDash.addEventListener("click", function(){
-    dash.style.display = "none";
+closeDash.addEventListener("click", function() {
+    dash.style.display = "none"
 })
