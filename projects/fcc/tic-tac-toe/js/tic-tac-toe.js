05620e06b7c025ed7e3e10e7670fc1c484908248
buoyantair <buoyantair@gmail.com>
2017-03-20 12:50:07 +0530

    Added close button and a dashboard

--- projects/fcc/tic-tac-toe/js/tic-tac-toe.js
+++ projects/fcc/tic-tac-toe/js/tic-tac-toe.js
@@ -1,14 +1,19 @@
 var gameboard = document.getElementById("game");
 var boxes = document.getElementsByClassName("box");
+var dash = document.getElementById("dash")
+var closeDash = document.getElementById("close");
+
 for(box in boxes){
     if(typeof boxes[box] == "object"){
         boxes[box].addEventListener("mouseenter", function(e){
-            e.srcElement.style.background = "rgb(" + Math.floor(Math.random() * 255) + ","+ Math.floor(Math.random() * 255) +","+ Math.floor(Math.random() * 255) + ")";
+            e.srcElement.style.background = "rgb(" + Math.floor(Math.random() * 255) + ","+ Math.floor(Math.random() * 255) +","+ Math.floor(Math.random() * 255) + ")";   ;
         });
         boxes[box].addEventListener("mouseleave", function(e){
             e.srcElement.style.background = "white";
         });
     }
 }
-/*
-*/
+
+closeDash.addEventListener("click", function(){
+    dash.style.display = "none";
+})
