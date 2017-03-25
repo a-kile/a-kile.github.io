df9cdd26eb309193ddb65bbbc8551aa6e0720142
buoyantair <buoyantair@gmail.com>
2017-03-25 11:47:56 +0530

    Ahhh minimaxz

--- projects/fcc/tic-tac-toe/old-version-smart/js/tic-tac-toe.js
+++ projects/fcc/tic-tac-toe/old-version-smart/js/tic-tac-toe.js
@@ -1,134 +1,138 @@
+// get out game play elements using javascript DOM methods
 var boxes = document.getElementsByClassName("box");
 var dash = document.getElementById("dash")
 var trigger = document.getElementById("playTrigger");
 
+// Some colors that will be used in the game 
 var colors = {
-    defSelCol: "rgba(50, 48, 49, 0.2)",
-    red: "#E3655B",
-    green: "#8CB369",
-    player: "#1F9E7C",
-    playerHover: "#109370",
-    computer: "#B244ED",
-    computerHover: "#6E2594",
-    defBoxCol : "#FFF"
+  defSelCol: "rgba(50, 48, 49, 0.2)",
+  red: "#E3655B",
+  green: "#8CB369",
+  human: "#1F9E7C",
+  playerHover: "#109370",
+  computer: "#B244ED",
+  computerHover: "#6E2594",
+  defBoxCol: "#FFF"
 }
-
-var state = [0,0,0,0,0,0,0,0,0];
+// Set default game state and make sure every box is empty or 0
+var state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+// By default the game starts as soon as the page loads
 var game = true;
 
-var HUMAN = false;
-var COMPUTER = true;
-
-var HUMVAL = -1;
-var COMVAL = 1;
-
+/* Win matrix is used in checkWin(); to compare with the current board state and decide if the player 
+won or not
+*/
 var winMatrix = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+/* 
+Human is going to be a true value, while computer is going to be a false value, this will later 
+help in altering the turns while checking through the nodes for the best possible value
+*/
+var human = true;
+var computer = false;
 
-function reset(){
-    for(var i=0; i < 9; i++){
-        boxes[i].style.background = "#fff";
-        state[i] = 0;
-    }
+/* 
+Player always tries to reduce the value of the node, while the computer always tries to maximize
+the value of the node.
+*/
+var humval = -1;
+var comval = 1;
 
-    game = true;
+// Quickly resets the whole game by changing the applied effects/colors & resetting the state to normal;
+function reset() {
+  for (var x = 0; x < 9; x++) {
+    boxes[x].style.background = colors.defBoxCol;
+    boxes[x].classList.remove("checked");
+    state[x] = 0;
+  }
 }
 
-function plot(clicked){
-    if(!game)
-        return;
-
-    for(var x=0; x < 9 ; x++){
-        if(boxes[x] == clicked && state[x] ==0){
-            set(x, HUMAN);
-            callAI();
-        }
+// Renders a box(adds a new class) from the given index & player values
+function renderBox(index, player) {
+  if (player == human) {
+    boxes[index].style.background = colors.human;
+    boxes[index].classList.add("checked");
+  } else {
+    boxes[index].style.background = colors.computer;
+    boxes[index].classList.add("checked");
+  }
+}
+/*
+  checkWin() iterates through each possible win value in winMatrix and then checks if any value in that 
+  array is not equal to the given value, if any value is not equal, that means the current state is not a win
+  state therefore the function throws back false, else if the iteration goes on to the end of atleast one 
+  win state without throwing false, then the function throws back true
+*/
+function checkWin(currentState, player) {
+  var playerVal = player == human ? humval : comval;
+  for (var x = 0; x < 8; x++) {
+    var win = true;
+    for (var y = 0; y < 3; y++) {
+      if (currentState[winMatrix[x][y]] != playerVal) {
+        win = false;
+        break;
+      }
     }
+    if (win) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function set(index, player){
-    if(!game)
-        return;
-
-    if(state[index] ==0){
-        if(player == HUMAN){
-            boxes[index].style.background = colors.player;
-            state[index] = HUMVAL;
-        } else {
-            boxes[index].style.background = colors.computer;
-            state[index] = COMVAL;
-        }
-
-        if(checkWin(state, player)){
-            game = false;
-        }
+/*
+checks if the box in the state which resembles the given index is empty, 
+if it is empty, it then checks which player's value is to be set, if it's a human then it sets
+the human value to that box and updates the state of the game & then renders the box with renderbox(), & vice versa. And then, it checks if the player won or not using checkWin();, if the player won, then the game ends, or else if the player hasn't won then checkFull() is run to see if the board is full or not, if it's full, the game ends with a draw, or else the game continues.
+*/
+function setBox(index, player) {
+  if (state[index] == 0) {
+    if (player == human) {
+      state[index] = humval;
+      renderBox(index, player);
+    } else {
+      state[index] = comval;
+      renderBox(index, player);
     }
+
+    if (winCheck(state, player) {
+        game = false;
+      }
+    }
+  }
+}
+// Checks if the board is full or not
+function checkFull(board) {
+  for (var x = 0; x < 9; x++) {
+    if (board[x] == 0) {
+      return false;
+    }
+  }
+  return false;
 }
 
-function checkWin(board, player){
-    var val = player == HUMAN ? HUMVAL : COMVAL;
-
-    for(var x=0; x < 8; x++){
-        var win = true;
-        for(var y=0; y < 3; y++){
-            if(board[winMatrix[x][y]] != val){
-                win = false;
-                break;
-            }
-        }
-        if(win){
-            return true;
-        }
-    }
-    return false;
+/*
+First, it checks if 
+*/
+function callAi(currentState, depth, player) {
+  
 }
 
-function checkFull(board){
-    for(var x=0; x < 9; x++){
-        if(board[x] ==0){
-            return false;
-        }
+function plot(box) {
+  if (game) {
+    for (var i = 0; i < 9; i++) {
+      if (boxes[i] == box && state[i] == 0) {
+        setBox(i, human);
+        callAi(state, computer);
+      }
     }
-    return false;
-}
-
-function callAI(){
-    aiturn(state, 0, COMPUTER);
-}
-
-function aiturn(board, depth, player){
-    if(checkWin(board, !player)){
-        return -10 + depth;
-    }
-    if(checkFull(board)){
-        return 0;
-    }
-
-    var value = player == HUMAN ? HUMVAL : COMVAL;
-    var max = -Infinity;
-    var index = 0;
-    for(var x=0; x < 9; x++){
-        if(board[x] ==0){
-            var newboard = board.slice();
-            newboard[x] = value;
-
-            var moveval = -aiturn(newboard, depth +1, !player);
-            if(moveval > max){
-                max = moveval;
-                index = x;
-            }
-        }
-    }
-    if(depth ==0){
-        set(index, COMPUTER);
-    }
-    return max;
+  }
 }
